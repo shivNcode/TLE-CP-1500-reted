04f13587711af75cd7e536b7ffe9cbd0c386a331
Shivansh <shivansh.123neekhra@gmail.com>
2026-05-06 03:19:40 +0530

    solve question on 1500 lunar new year and wander

--- D_Lunar_New_Year_and_a_Wander.cpp
+++ D_Lunar_New_Year_and_a_Wander.cpp
@@ -1,40 +1,57 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <deque>
-#include <random>
-#include <chrono>
-#include <unordered_map>
-#include <map>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
+// #define int long long
 #define INF (int)1e18
 #define all(x) (x).begin(), (x).end()
 #define sz(x) (int)(x).size()
 
 mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+vector<vector<int>> arr;
+vector<int> vis;
+priority_queue<int, vector<int>, greater<int>> pq;
 
+void bfs(int x){
+    cout << x << " ";
+    vis[x] = 1;
+
+    for(int i : arr[x]){
+        if(!vis[i]){
+            vis[i] = 1;
+            pq.push(i);
+        }
+    }
+}
 void Solve()
 {
-    int n; cin >> n;
-    vector<int> arr(n);
-    for(int i=0;i<n;i++){
-        cin >> arr[i];
+
+    int n , m; cin >> n >> m;
+    arr.assign(n+1 , {});
+    vis.assign(n+1 , 0);
+    for(int i=0;i<m;i++){
+        int a , b; cin >> a >> b;
+        arr[a].push_back(b);
+        arr[b].push_back(a);
     }
+    pq.push(1);
+    while(!pq.empty()){
+        int p = pq.top();
+        pq.pop();
+        bfs(p);
+    }
+    cout << endl;
 
 
 }
 
 
-int32_t main()
+int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t = 1;
-    cin >> t;
+    //cin >> t;
     while (t--)
     {
         Solve();
